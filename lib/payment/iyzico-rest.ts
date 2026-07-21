a843dabbe8e178d0cea7a3d420a3ae4349af4b9a
Senol Kara <karasenol37@gmail.com>
2026-07-22 00:21:54 +0300

    Fix iyzico v2 authorization

--- lib/payment/iyzico-rest.ts
+++ lib/payment/iyzico-rest.ts
@@ -2,41 +2,62 @@ import CryptoJS from "crypto-js";
 
 
 export function generateIyzipayAuthorization(
-  uri: string,
-  requestBody: string
-) {
+  uri:string,
+  requestBody:string
+){
 
   const apiKey =
     process.env.IYZICO_API_KEY!;
+
 
   const secretKey =
     process.env.IYZICO_SECRET_KEY!;
 
 
+
   const randomString =
     CryptoJS.lib.WordArray.random(16)
-      .toString();
+      .toString(
+        CryptoJS.enc.Hex
+      );
 
 
-  const hash =
+
+
+  const signature =
     CryptoJS.HmacSHA256(
+
       randomString +
       uri +
       requestBody,
+
       secretKey
+
     )
     .toString(
       CryptoJS.enc.Base64
     );
 
 
+
+
+
   return {
 
+
     Authorization:
-      `IYZWSv2 ${apiKey}:${randomString}:${hash}`,
+      `IYZWSv2 ${apiKey}:${signature}`,
+
+
+
+    "x-iyzi-rnd":
+      randomString,
+
+
 
     "Content-Type":
       "application/json"
+
 
   };
 
