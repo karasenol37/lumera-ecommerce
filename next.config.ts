4a780effd4727e1dae8d4f3146608a5be6b7dacc
Senol Kara <karasenol37@gmail.com>
2026-07-23 13:48:32 +0300

    add asynckit dependency

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
 
     "/api/payment/create": [
+      "./node_modules/asynckit/**",
       "./node_modules/@postman/form-data/**",
       "./node_modules/extsprintf/**",
 "./node_modules/verror/**",
@@ -84,6 +85,7 @@ const nextConfig: NextConfig = {
 
 
     "/api/payment/callback": [
+      "./node_modules/asynckit/**",
       "./node_modules/@postman/form-data/**",
       "./node_modules/extsprintf/**",
 "./node_modules/verror/**",
